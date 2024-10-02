9a4f08b8cd2ba1162d907a6a6ea8070c22fb1abd
Kirill Chizhevskiy <chizhevskiy.k@compmechlab.ru>
2024-10-02 14:49:53 +0300

    updated tests

--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import constants
 from locators import Locators
 
 
@@ -8,33 +9,33 @@ class TestPersonalAccount:
     def test_navigate_to_personal_account(self, driver, authorization):
         driver.find_element(*Locators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 10).until(
-            EC.url_to_be("https://stellarburgers.nomoreparties.site/account/profile")
+            EC.url_to_be(constants.PERSONAL_ACCOUNT_URL)
         )
-        assert driver.current_url == "https://stellarburgers.nomoreparties.site/account/profile"
+        assert driver.current_url == constants.PERSONAL_ACCOUNT_URL
 
     def test_navigate_from_personal_account_to_constructor(self, driver, authorization):
         driver.find_element(*Locators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 10).until(
-            EC.url_to_be("https://stellarburgers.nomoreparties.site/account/profile")
+            EC.url_to_be(constants.PERSONAL_ACCOUNT_URL)
         )
         driver.find_element(*Locators.CONSTRUCTOR_BUTTON).click()
-        assert driver.current_url == 'https://stellarburgers.nomoreparties.site/'
+        assert driver.current_url == constants.HOMEPAGE_URL
 
     def test_navigate_from_personal_account_to_logo(self, driver, authorization):
         driver.find_element(*Locators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 10).until(
-            EC.url_to_be("https://stellarburgers.nomoreparties.site/account/profile")
+            EC.url_to_be(constants.PERSONAL_ACCOUNT_URL)
         )
         driver.find_element(*Locators.LOGO).click()
-        assert driver.current_url == 'https://stellarburgers.nomoreparties.site/'
+        assert driver.current_url == constants.HOMEPAGE_URL
 
     def test_logout_from_personal_account(self, driver, authorization):
         driver.find_element(*Locators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 10).until(
-            EC.url_to_be("https://stellarburgers.nomoreparties.site/account/profile")
+            EC.url_to_be(constants.PERSONAL_ACCOUNT_URL)
         )
         driver.find_element(*Locators.LOGOUT_BUTTON).click()
         WebDriverWait(driver, 10).until(
-            EC.url_to_be("https://stellarburgers.nomoreparties.site/login")
+            EC.url_to_be(constants.LOGIN_URL)
         )
-        assert driver.current_url == 'https://stellarburgers.nomoreparties.site/login'
+        assert driver.current_url == constants.LOGIN_URL
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,34 +1,35 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import Locators
+import constants
 
 
 class TestRegistration:
 
     def test_registration_successful(self, driver):
-        driver.get("https://stellarburgers.nomoreparties.site/")
+        driver.get(constants.HOMEPAGE_URL)
 
         driver.find_element(*Locators.LOGIN_BUTTON).click()
         driver.find_element(*Locators.REGISTRATION_BUTTON).click()
 
-        driver.find_element(*Locators.REG_NAME_INPUT).send_keys('Кирилл')
-        driver.find_element(*Locators.REG_EMAIL_INPUT).send_keys('chizhevskiy_kirill_14_953@yandex.ru')
-        driver.find_element(*Locators.REG_PASSWORD_INPUT).send_keys('123123')
+        driver.find_element(*Locators.REG_NAME_INPUT).send_keys(constants.REG_NAME)
+        driver.find_element(*Locators.REG_EMAIL_INPUT).send_keys(constants.REG_EMAIL)
+        driver.find_element(*Locators.REG_PASSWORD_INPUT).send_keys(constants.REG_PASSWORD)
         driver.find_element(*Locators.REG_APPLY_BUTTON).click()
 
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located(Locators.LOGIN_PAGE_TITLE))
-        assert driver.current_url == "https://stellarburgers.nomoreparties.site/login"
+        assert driver.current_url == constants.LOGIN_URL
 
     def test_registration_with_invalid_password(self, driver):
-        driver.get("https://stellarburgers.nomoreparties.site/")
+        driver.get(constants.HOMEPAGE_URL)
 
         driver.find_element(*Locators.LOGIN_BUTTON).click()
         driver.find_element(*Locators.REGISTRATION_BUTTON).click()
 
-        driver.find_element(*Locators.REG_NAME_INPUT).send_keys('Кирилл')
-        driver.find_element(*Locators.REG_EMAIL_INPUT).send_keys('chizhevskiy_kirill_14_123@yandex.ru')
-        driver.find_element(*Locators.REG_PASSWORD_INPUT).send_keys('123')
+        driver.find_element(*Locators.REG_NAME_INPUT).send_keys(constants.REG_NAME)
+        driver.find_element(*Locators.REG_EMAIL_INPUT).send_keys(constants.REG_EMAIL)
+        driver.find_element(*Locators.REG_PASSWORD_INPUT).send_keys(constants.REG_INVALID_PASSWORD)
         driver.find_element(*Locators.REG_APPLY_BUTTON).click()
 
         invalid_password = WebDriverWait(driver, 10).until(
